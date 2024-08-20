765aaca697c1f710f137e79fbdaf1078884094a0
miles453864 <29894500+NostalgicLorikeet@users.noreply.github.com>
2024-08-20 05:02:57 -0400

    Can I have the Nitrus today

--- docs/assets/scripts/content/sidebar.js
+++ docs/assets/scripts/content/sidebar.js
@@ -14,6 +14,7 @@ new sbSection("Archives and mirrors","archives.png","true","true");
 		new sbELink("Mirror on file server","https://files.hitscan.org/files_two/archives/gms1.4/dadiospice/","Another mirror but on the file server instead, unmodified from the original","false","true");
 new sbSection("Old stuff","old_stuff.png","false","true");
 	new sbLink("NostalgicLorikeet.github.io (2017)","https://files.hitscan.org/files_two/archives/NostalgicLorikeet.github.io/","How this site (sort of?? I mean that URL redirects to its own domain now) looked like in 2017","true","true");
+	new sbLink("Old YouTube internet story videos (2019-2021)","https://www.youtube.com/playlist?list=PLoCLvSy5WJvGi19_KGkAEGTUAEojbBFGy","Old vids I made about stuff that happened online, a lot of them are pretty dated now","true","true");
 	new sbLink("hitscan.neocities.org (2022)","https://hitscan.neocities.org","This never went anywhere unfortunately","true","true");
 		lastEntry.altIcon = "https://neocities.org/favicon.ico";
 	new sbLink("hitscan.org (2023)","https://files.hitscan.org/files_two/archives/hitscan%20dot%20cee%20cee/","How this site looked for like a year","true","true");
