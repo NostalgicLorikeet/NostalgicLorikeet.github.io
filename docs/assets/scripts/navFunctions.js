9c6202c36ccdd7d1035328275d30c5753422fbb6
miles453864 <29894500+NostalgicLorikeet@users.noreply.github.com>
2024-08-20 04:34:26 -0400

    oops

--- docs/assets/scripts/navFunctions.js
+++ docs/assets/scripts/navFunctions.js
@@ -48,12 +48,8 @@ class sbLink {
 		this.onSitemap = "true";
 		this.onSitemap = onSitemap;
 		if (url.indexOf("https://") == 0) {
-			this.urlA = this.url.substring(8);
-			if (this.url.indexOf(this.url.length - 1) != "/") {
-				this.altIcon = "https://" + this.urlA + "/favicon.ico";
-			} else {
-				this.altIcon = "https://" + this.urlA.substring(0,this.urlA.indexOf("/")) + "/favicon.ico";
-			}
+			this.urlProcessor = new URL(this.url);
+			this.altIcon = this.urlProcessor.hostname + "/favicon.ico";
 		}
 		this.externals = [];
 		lastEntry = this;
@@ -69,7 +65,7 @@ class sbELink {
 		this.onSidebar = onSidebar;
 		this.onSitemap = onSitemap;
 		if (url.indexOf("https://") == 0) {
-			this.urlProcessor = new URL("this.url");
+			this.urlProcessor = new URL(this.url);
 			this.altIcon = this.urlProcessor.hostname + "/favicon.ico";
 		}
 		lastEntry.externals.push(this);
